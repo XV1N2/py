aa5cc4b394226e0037d1d1fb89e8db46315d054a
XV1N2 <95734686+XV1N2@users.noreply.github.com>
2023-02-12 04:23:13 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+import os
+import random
+
+for i in range():
+    ans = random.randint(0.100)
+    chc = int(input("Enter Your Choice: "))
+    if chc == ans:
+        print(f"GreatJob, Choice {chc} Answer {ans}")
+        os.system("PAUSE")
+    else:
+        print(f"Incorrect, Choice {chc} Answer {ans}")
+        os.system("PAUSE")
+    return
